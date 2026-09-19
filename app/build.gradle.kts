9d1928eb3878b84b22015256dcb15da185741c6d
arlisilver80-create <arlisilver80@gmail.com>
2026-09-19 12:18:45 +0300

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.arlimusik.inflator"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.arlimusik.inflator"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 21
+        versionName = "2.1-tablet"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
